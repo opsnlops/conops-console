562349b50a99cd544be8f08d11da7447d9f849b2
April White <april@opsnlops.io>
2025-01-04 14:07:44 -0800

    Initial Commit

---
+++ conops-console/conops_consoleApp.swift
@@ -0,0 +1,32 @@
+//
+//  conops_consoleApp.swift
+//  conops-console
+//
+//  Created by April White on 1/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct conops_consoleApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ conops-consoleTests/conops_consoleTests.swift
@@ -0,0 +1,16 @@
+//
+//  conops_consoleTests.swift
+//  conops-consoleTests
+//
+//  Created by April White on 1/4/25.
+//
+
+import Testing
+
+struct conops_consoleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
