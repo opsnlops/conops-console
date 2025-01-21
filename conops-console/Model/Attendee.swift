a8cb2fc839442e59347d29863fa16757042232a5
April White <april@opsnlops.io>
2025-01-21 10:24:47 -0800

    Break up Attendee into the Swift and DTO model

--- conops-console/Model/Attendee.swift
+++ conops-console/Model/Attendee.swift
@@ -7,93 +7,150 @@
 //
 
 import Foundation
+import SwiftData
 
-struct Attendee: Codable, Identifiable, Comparable, Hashable {
-    let id: UUID
-    let lastModified: Date
-    let active: Bool
-    let badgeNumber: UInt32
-    let firstName: String
-    let lastName: String
-    let badgeName: String
-    let membershipLevel: MembershipLevel
-    let birthday: Date
-    let addressLine1: String
-    let addressLine2: String?
-    let city: String
-    let state: String
-    let postalCode: String
-    let shirtSize: String?
-    let emailAddress: String
-    let emergencyContact: String?
-    let phoneNumber: String?
-    let registrationDate: Date
-    let checkInTime: Date?
-    let staff: Bool
-    let dealer: Bool
-    let codeOfConductAccepted: Bool
-    let secretCode: String?
-    let transactions: [Transaction]
+@Model
+final class Attendee {
+    var id: UUID
+    var lastModified: Date
+    var active: Bool
+    var badgeNumber: UInt32
+    var firstName: String
+    var lastName: String
+    var badgeName: String
+    var membershipLevel: MembershipLevel
+    var birthday: Date
+    var addressLine1: String
+    var addressLine2: String?
+    var city: String
+    var state: String
+    var postalCode: String
+    var shirtSize: String?
+    var emailAddress: String
+    var emergencyContact: String?
+    var phoneNumber: String?
+    var registrationDate: Date
+    var checkInTime: Date?
+    var staff: Bool
+    var dealer: Bool
+    var codeOfConductAccepted: Bool
+    var secretCode: String?
+    var transactions: [Transaction]
 
-    enum CodingKeys: String, CodingKey {
-        case id
-        case lastModified = "last_modified"
-        case active
-        case badgeNumber = "badge_number"
-        case firstName = "first_name"
-        case lastName = "last_name"
-        case badgeName = "badge_name"
-        case membershipLevel = "membership_level"
-        case birthday
-        case addressLine1 = "address_line_1"
-        case addressLine2 = "address_line_2"
-        case city
-        case state
-        case postalCode = "postal_code"
-        case shirtSize = "shirt_size"
-        case emailAddress = "email_address"
-        case emergencyContact = "emergency_contact"
-        case phoneNumber = "phone_number"
-        case registrationDate = "registration_date"
-        case checkInTime = "check_in_time"
-        case staff
-        case dealer
-        case codeOfConductAccepted = "code_of_conduct_accepted"
-        case secretCode = "secret_code"
-        case transactions
+    init(
+        id: UUID,
+        lastModified: Date,
+        active: Bool,
+        badgeNumber: UInt32,
+        firstName: String,
+        lastName: String,
+        badgeName: String,
+        membershipLevel: MembershipLevel,
+        birthday: Date,
+        addressLine1: String,
+        addressLine2: String?,
+        city: String,
+        state: String,
+        postalCode: String,
+        shirtSize: String?,
+        emailAddress: String,
+        emergencyContact: String?,
+        phoneNumber: String?,
+        registrationDate: Date,
+        checkInTime: Date?,
+        staff: Bool,
+        dealer: Bool,
+        codeOfConductAccepted: Bool,
+        secretCode: String?,
+        transactions: [Transaction]
+    ) {
+        self.id = id
+        self.lastModified = lastModified
+        self.active = active
+        self.badgeNumber = badgeNumber
+        self.firstName = firstName
+        self.lastName = lastName
+        self.badgeName = badgeName
+        self.membershipLevel = membershipLevel
+        self.birthday = birthday
+        self.addressLine1 = addressLine1
+        self.addressLine2 = addressLine2
+        self.city = city
+        self.state = state
+        self.postalCode = postalCode
+        self.shirtSize = shirtSize
+        self.emailAddress = emailAddress
+        self.emergencyContact = emergencyContact
+        self.phoneNumber = phoneNumber
+        self.registrationDate = registrationDate
+        self.checkInTime = checkInTime
+        self.staff = staff
+        self.dealer = dealer
+        self.codeOfConductAccepted = codeOfConductAccepted
+        self.secretCode = secretCode
+        self.transactions = transactions
+    }
+}
+
+// MARK: - DTO conversions
+extension Attendee {
+    static func fromDTO(_ dto: AttendeeDTO) -> Attendee {
+        return Attendee(
+            id: dto.id,
+            lastModified: dto.lastModified,
+            active: dto.active,
+            badgeNumber: dto.badgeNumber,
+            firstName: dto.firstName,
+            lastName: dto.lastName,
+            badgeName: dto.badgeName,
+            membershipLevel: dto.membershipLevel,
+            birthday: dto.birthday,
+            addressLine1: dto.addressLine1,
+            addressLine2: dto.addressLine2,
+            city: dto.city,
+            state: dto.state,
+            postalCode: dto.postalCode,
+            shirtSize: dto.shirtSize,
+            emailAddress: dto.emailAddress,
+            emergencyContact: dto.emergencyContact,
+            phoneNumber: dto.phoneNumber,
+            registrationDate: dto.registrationDate,
+            checkInTime: dto.checkInTime,
+            staff: dto.staff,
+            dealer: dto.dealer,
+            codeOfConductAccepted: dto.codeOfConductAccepted,
+            secretCode: dto.secretCode,
+            transactions: dto.transactions
+        )
     }
 
-    static func < (lhs: Attendee, rhs: Attendee) -> Bool {
-        lhs.badgeName.localizedCaseInsensitiveCompare(rhs.badgeName) == .orderedAscending
-    }
-
-    static func mock() -> Attendee {
-        Attendee(
-            id: UUID(),
-            lastModified: Date(),
-            active: true,
-            badgeNumber: 1234,
-            firstName: "Mock",
-            lastName: "Attendee",
-            badgeName: "Mocky",
-            membershipLevel: MembershipLevel.mock(),
-            birthday: Date(),
-            addressLine1: "123 Mock St",
-            addressLine2: "Apt 4B",
-            city: "Mock City",
-            state: "WA",
-            postalCode: "12345",
-            shirtSize: "Medium",
-            emailAddress: "mock@example.com",
-            emergencyContact: "Mock Contact",
-            phoneNumber: "123-456-7890",
-            registrationDate: Date(),
-            checkInTime: Date().addingTimeInterval(-3600),
-            staff: false,
-            dealer: false,
-            codeOfConductAccepted: true,
-            secretCode: "MockSecret",
-            transactions: [Transaction.mock()]
+    func toDTO() -> AttendeeDTO {
+        return AttendeeDTO(
+            id: self.id,
+            lastModified: self.lastModified,
+            active: self.active,
+            badgeNumber: self.badgeNumber,
+            firstName: self.firstName,
+            lastName: self.lastName,
+            badgeName: self.badgeName,
+            membershipLevel: self.membershipLevel,
+            birthday: self.birthday,
+            addressLine1: self.addressLine1,
+            addressLine2: self.addressLine2,
+            city: self.city,
+            state: self.state,
+            postalCode: self.postalCode,
+            shirtSize: self.shirtSize,
+            emailAddress: self.emailAddress,
+            emergencyContact: self.emergencyContact,
+            phoneNumber: self.phoneNumber,
+            registrationDate: self.registrationDate,
+            checkInTime: self.checkInTime,
+            staff: self.staff,
+            dealer: self.dealer,
+            codeOfConductAccepted: self.codeOfConductAccepted,
+            secretCode: self.secretCode,
+            transactions: self.transactions
         )
     }
 }
---
+++ conops-console/Model/DTO/AttendeeDTO.swift
@@ -0,0 +1,99 @@
+//
+//  AttendeeDTO.swift
+//  Conops Console
+//
+//  Created by April White on 1/21/25.
+//  Copyright © 2025 April's Creature Workshop. All rights reserved.
+//
+
+import Foundation
+
+struct AttendeeDTO: Codable, Identifiable, Comparable, Hashable, Sendable {
+    let id: UUID
+    let lastModified: Date
+    let active: Bool
+    let badgeNumber: UInt32
+    let firstName: String
+    let lastName: String
+    let badgeName: String
+    let membershipLevel: MembershipLevel
+    let birthday: Date
+    let addressLine1: String
+    let addressLine2: String?
+    let city: String
+    let state: String
+    let postalCode: String
+    let shirtSize: String?
+    let emailAddress: String
+    let emergencyContact: String?
+    let phoneNumber: String?
+    let registrationDate: Date
+    let checkInTime: Date?
+    let staff: Bool
+    let dealer: Bool
+    let codeOfConductAccepted: Bool
+    let secretCode: String?
+    let transactions: [Transaction]
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case lastModified = "last_modified"
+        case active
+        case badgeNumber = "badge_number"
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case badgeName = "badge_name"
+        case membershipLevel = "membership_level"
+        case birthday
+        case addressLine1 = "address_line_1"
+        case addressLine2 = "address_line_2"
+        case city
+        case state
+        case postalCode = "postal_code"
+        case shirtSize = "shirt_size"
+        case emailAddress = "email_address"
+        case emergencyContact = "emergency_contact"
+        case phoneNumber = "phone_number"
+        case registrationDate = "registration_date"
+        case checkInTime = "check_in_time"
+        case staff
+        case dealer
+        case codeOfConductAccepted = "code_of_conduct_accepted"
+        case secretCode = "secret_code"
+        case transactions
+    }
+
+    static func < (lhs: AttendeeDTO, rhs: AttendeeDTO) -> Bool {
+        lhs.badgeName.localizedCaseInsensitiveCompare(rhs.badgeName) == .orderedAscending
+    }
+
+    static func mock() -> AttendeeDTO {
+        return AttendeeDTO(
+            id: UUID(),
+            lastModified: Date(),
+            active: true,
+            badgeNumber: 1234,
+            firstName: "Mock",
+            lastName: "Attendee",
+            badgeName: "Mocky",
+            membershipLevel: MembershipLevel.mock(),
+            birthday: Date(),
+            addressLine1: "123 Mock St",
+            addressLine2: "Apt 4B",
+            city: "Mock City",
+            state: "WA",
+            postalCode: "12345",
+            shirtSize: "Medium",
+            emailAddress: "mock@example.com",
+            emergencyContact: "Mock Contact",
+            phoneNumber: "123-456-7890",
+            registrationDate: Date(),
+            checkInTime: Date().addingTimeInterval(-3600),
+            staff: false,
+            dealer: false,
+            codeOfConductAccepted: true,
+            secretCode: "MockSecret",
+            transactions: [Transaction.mock()]
+        )
+    }
+}
